32572abf6d88250dee1820d18be85a65d4905094
Andrew <alekseevandrew105@gmail.com>
2023-08-05 12:01:12 +0300

    homework18

---
+++ src/Lesson34/Animal.java
@@ -0,0 +1,22 @@
+package Lesson34;
+
+public class Animal {
+    private int id;
+
+    public Animal(int id) {
+        this.id = id;
+    }
+
+    public Animal() {
+
+    }
+
+    public String toString() {
+        return String.valueOf(id);
+    }
+
+    public void eat() {
+        System.out.println("Animal is eating");
+
+    }
+}
